4490f40f347ee1ea07b7791f5c29270ace545456
Unknown <mariusmeschter99@gmail.com>
2016-11-30 20:35:02 +0100

    .........

--- commands/misc/verify.js
+++ commands/misc/verify.js
@@ -3,9 +3,9 @@ const { Command } = require('discord.js-commando');
 module.exports = class PingCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'wetter',
+			name: 'verify',
 			group: 'misc',
-			memberName: 'wetter',
+			memberName: 'verify',
 			description: 'Soon'
 		});
 	}
